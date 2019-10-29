a2f9a3d36043d3c6f252b22d40334808fe12a209
leandro <leandro.vicente98@outlook.com>
2019-10-29 03:03:39 -0300

    publish website umbler

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ var knex = require("knex")({
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
-
-server.listen(8080, function() {
+server.listen(3000, function() {
   console.log("%s listening at %s", server.name, server.url);
 });
 
