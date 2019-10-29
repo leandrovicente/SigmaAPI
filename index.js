ec0420341b6c95b07602e6a7cde0a4a791a7de4b
leandro <leandro.vicente98@outlook.com>
2019-10-29 02:39:33 -0300

    Configurado todas as rotas para tblusuario

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var knex = require("knex")({
   connection: {
     host: "mysql669.umbler.com",
     user: "sigma",
-    password: "",
+    password: "sigmarh123",
     port: "41890",
     database: "rhsigma"
   }
@@ -25,18 +25,59 @@ server.listen(8080, function() {
   console.log("%s listening at %s", server.name, server.url);
 });
 
-// rotas REST
-
-server.get("/", (req, res, next) => {
-  knex("rest").then(dados => {
+// rotas Usuarios
+server.get("/usuarios", (req, res, next) => {
+  knex("tblusuario").then(dados => {
     res.send(dados);
   }, next);
 });
 
-server.post("/create", (req, res, next) => {
-  knex("rest")
+server.post("/usuarios/create", (req, res, next) => {
+  knex("tblusuario")
     .insert(req.body)
     .then(dados => {
       res.send(dados);
+    }, next);
+});
+
+server.get("usuarios/show/:id", (req, res, next) => {
+  const { id } = req.params;
+  knex("tblusuario")
+    .where("id", id)
+    .first()
+    .then(dados => {
+      if (!dados)
+        return res.send(
+          new errs.BadRequestError("não possui usuario com esse id")
+        );
+      res.send(dados);
+    }, next);
+});
+
+server.put("usuarios/update/:id", (req, res, next) => {
+  const { id } = req.params;
+  knex("tblusuario")
+    .where("id", id)
+    .update(req.body)
+    .then(dados => {
+      if (!dados)
+        return res.send(
+          new errs.BadRequestError("não possui usuario com esse id")
+        );
+      res.send("Dados Atualizado");
+    }, next);
+});
+
+server.del("usuarios/delete/:id", (req, res, next) => {
+  const { id } = req.params;
+  knex("tblusuario")
+    .where("id", id)
+    .delete()
+    .then(dados => {
+      if (!dados)
+        return res.send(
+          new errs.BadRequestError("não possui usuario com esse id")
+        );
+      res.send("Dados Excluidos");
     }, next);
 });
